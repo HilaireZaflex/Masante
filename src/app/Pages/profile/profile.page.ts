40c2f06e3bdeff3ed5649a46d8c3b5f3c45ec709
Hilaire Vangmbono Daliwa <hilaire.daliwa@odc.local>
2022-02-15 15:05:30 +0000

    nouveau front

--- src/app/Pages/profile/profile.page.ts
+++ src/app/Pages/profile/profile.page.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { User } from 'src/app/user';
 import { UserService } from 'src/app/service/user.service';
+import { Router, NavigationExtras } from '@angular/router';
 
 @Component({
   selector: 'app-profile',
@@ -8,6 +9,7 @@ import { UserService } from 'src/app/service/user.service';
   styleUrls: ['./profile.page.scss'],
 })
 export class ProfilePage implements OnInit {
+  subjects;
   public users: User[];
   InfoUser: any;
 
@@ -15,7 +17,33 @@ export class ProfilePage implements OnInit {
 
   ngOnInit() {
     this.InfoUser=JSON.parse(localStorage.getItem('user'));
-    console.log(this.InfoUser)
+    console.log(this.InfoUser);
+
+    this.subjects = [
+      {
+        name : 'All'
+      },
+      {
+        img: 'assets/sport.png',
+        name: "Sport"
+      },
+      {
+        img: 'assets/sport1.png',
+        name: "Sport"
+      },
+      {
+        img: 'assets/sport3.png',
+        name: "Sport"
+      },
+      {
+        img: 'assets/sport.png',
+        name: "Sport"
+      }
+    ];
+  }
+
+  goToSubject(){
+
   }
 
 }
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,17 @@ import { Component } from '@angular/core';
   styleUrls: ['app.component.scss'],
 })
 export class AppComponent {
+  public pages: any[] = [
+    {title: 'Accueil', url: '/accueil', icon: 'home'},
+    {title: 'Docteur', url: '/docteur', icon: 'document-lock'},
+    {title: 'Conseil', url: '/conseil-sante', icon: 'information-circle'},
+    {title: 'Profile', url: '/profile', icon: 'person'},
+    {title: 'Se déconnecter', url: '', icon: 'log-out', route: true},
+  ];
   constructor() {}
+
+  signOut() {
+    console.log('signout');
+    
+  }
 }
