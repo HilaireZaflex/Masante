53a5cd3fd2b28bd35ac56683fc9e625f942675a7
madymadesc <madymadesc@gmail.com>
2022-02-18 19:13:30 +0000

    ajout de l'algorithme

--- src/app/Pages/suivi-diabete/suivi-diabete.page.ts
+++ src/app/Pages/suivi-diabete/suivi-diabete.page.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
-import { Platform } from '@ionic/angular';
+import { NgForm } from '@angular/forms';
+import { Router } from '@angular/router';
+import { NavController, Platform } from '@ionic/angular';
+import { Donnee } from 'src/app/model/donnee';
+import { UserService } from 'src/app/service/user.service';
 
 @Component({
   selector: 'app-suivi-diabete',
@@ -8,14 +12,47 @@ import { Platform } from '@ionic/angular';
 })
 export class SuiviDiabetePage implements OnInit {
   rangeVal: string;
+  rangeValu:string;
 
-  constructor(public platform: Platform) { 
+  donnee = new Donnee()
+    valeurA:any;
+    valeurC:any;
+    unite:any;
+    diabete:any;
+
+  constructor(
+    public platform: Platform,
+    private service: UserService,   
+    private router: Router,
+    private navCrr : NavController
+    ) { 
     this.platform.ready().then(()=>{
       this.rangeVal = "50";
     })
+    this.platform.ready().then(()=>{
+      this.rangeValu = "0.7"
+    })
   }
 
-  ngOnInit() {
-  }
+  ngOnInit() {}
+
+  async calculD(forms:NgForm){
+
+    this.valeurA=forms.value['valeurA'];
+    this.valeurC=forms.value['valeurC'];
+    this.unite=forms.value['unite'];
+    
+
+    this.donnee.valeurA=this.valeurA;
+    this.donnee.valeurC=this.valeurC;
+    this.donnee.unite=this.unite;
+   
+    this.service.calculDiabete(this.donnee).subscribe((data:any)=>{
+      this.diabete=data;
+      console.log("votre resuta"+ data)
+      this.router.navigate(['tabs'])
+    })
+    
+   }
 
 }
--- src/app/Pages/suivi/suivi.page.ts
+++ src/app/Pages/suivi/suivi.page.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { Router } from '@angular/router';
+import { NavController } from '@ionic/angular';
+import { Donnee } from 'src/app/model/donnee';
+import { UserService } from 'src/app/service/user.service';
 
 
 @Component({
@@ -7,11 +12,47 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./suivi.page.scss'],
 })
 export class SuiviPage implements OnInit {
+    donnee = new Donnee()
+    valeurA:any;
+    valeurB:any;
+    valeurC:any;
+    unite:any;
+    malad:any;
+    resultat:any;
+    calcul:any;
 
-  constructor() { }
+  constructor(
+    private service: UserService,   
+    private router: Router,
+    private navCrr : NavController
+  ) { }
   
 
-  ngOnInit() {
-  }
+  ngOnInit():void {}
+
+  async Calcul(forms:NgForm){
+
+    this.valeurA=forms.value['valeurA'];
+    this.valeurB=forms.value['valeurB'];
+    this.valeurC=forms.value['valeurC'];
+    this.unite=forms.value['unite'];
+    this.malad=forms.value['malad'];
+    this.resultat=forms.value['resultat'];
+
+    this.donnee.valeurA=this.valeurA;
+    this.donnee.valeurB=this.valeurB;
+    this.donnee.valeurC=this.valeurC;
+    this.donnee.unite=this.unite;
+    this.donnee.malad=this.malad;
+    this.donnee.resultat=this.resultat;
+   
+    this.service.calculDonnee(this.donnee).subscribe((data:any)=>{
+      this.calcul=data;
+      console.log("votre resuta"+ data)
+      this.router.navigate(['accuiel'])
+    })
+    
+   }
+
 
 }
