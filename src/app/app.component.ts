7a2902bec31a890b4c755fd3949a10b612d20bc2
madymadesc <madymadesc@gmail.com>
2022-03-11 22:20:41 +0000

    menu page design

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -7,16 +8,26 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   public pages: any[] = [
-    {title: 'Accueil', url: '/tabs', icon: 'home'},
-    {title: 'Docteur', url: '/docteur', icon: 'document-lock'},
-    {title: 'Conseil', url: '/conseil-sante', icon: 'information-circle'},
-    {title: 'Profile', url: '/profile', icon: 'person'},
-    {title: 'Se déconnecter', url: '', icon: 'log-out', route: true},
+    {title: 'Accueil', url: '/tabs', icon: 'home',image:'../assets/iconImage/home.png'},
+    {title: 'Données', url: '/dd', icon: 'person', image:'../assets/iconImage/database.png'},
+    {title: 'Conseils', url: '/conseil-sante', icon: 'information-circle',image:'../assets/iconImage/pharmacy.png'},
+    {title: 'Medecins', url: '/docteur', icon: 'person', image:'../assets/iconImage/doctor.png'},
+    {title: 'A propos', url: '/dd', icon: 'person', image:'../assets/iconImage/about.png'},
+    {title: 'Déconnexion', url: '', icon: 'log-out', image:'../assets/iconImage/logout.png', route: true},
   ];
-  constructor() {}
+  InfoUser :any;
+  constructor(
+    private router : Router
+  ) {}
+
+  ngOnInit() {
+    
+    this.InfoUser=JSON.parse(localStorage.getItem('user'));
+    }
 
   signOut() {
-    console.log('signout');
-    
+    localStorage.removeItem('user');
+    window.location.href="/";
+    history.forward();
   }
 }
